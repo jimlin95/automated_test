f4b166229a1a29574c82139b7f8a74bcd7dc9f88
Jim Lin <jim_lin@quantatw.com>
2015-04-27 16:36:22 +0800

    Add log for cts preset process
    Signed-off-by: Jim Lin <jim_lin@quantatw.com>

--- cts/cts-preset.py
+++ cts/cts-preset.py
@@ -33,19 +33,35 @@ device, serialno = ViewClient.connectToDeviceOrExit()
 vc = ViewClient(device=device, serialno=serialno)
 ud = UiDevice(vc=vc)
 vc.sleep(1)
+print u'Start to Skip setup wizzard'
 skip_setupwizzard(vc)
+print u'Skip setup wizzard --- Done'
 # Press the HOME button to start the test from the home screen
 device.press('KEYCODE_HOME','DOWN_AND_UP')
 
 #Change language to English (United States)
+print u'Start to Change Language to English'
 ud.changeLanguage('en-rUS')
+print u'Change Language to English --- Done'
+print u'Start to Change the settings in Developer'
 ChangeDeveloper_settings(vc)
+print u'Change the settings in Developer --- Done'
+print u'Start to Change the settings in Security'
 setSecurity(vc)
+print u'Change the settings in Security --- Done'
+print u'Start to Change the TimeZone to GMT + 00:00'
 setTimezone(vc)
+print u'Change the TimeZone to GMT + 00:00 --- Done'
+print u'Start to Connect device to Wifi'
 SetWifiConnect(vc,u'dlink-549',u'38017549')
+print u'Connect device to Wifi --- Done'
+print u'Start to Modify the network to fit CTS\'s requirement'
 ModifyNetwork(vc,u'dlink-549')
+print u'Modify the network to fit CTS\'s requirement --- Done'
 # Press the HOME button to start the test from the home screen
 device.press('KEYCODE_HOME','DOWN_AND_UP')
+print u'Start to Run Chrome browser & confirm the settings'
 set_chrome(vc)
+print u'Run Chrome browser & confirm the settings --- Done'
 device.press('KEYCODE_HOME','DOWN_AND_UP')
 
